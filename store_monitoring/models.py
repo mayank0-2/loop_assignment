567a59de8ebae52f4d96ec7fa8547b13e6a04d18
mayank0-2 <mayank02091999@gmail.com>
2024-05-01 16:14:11 +0530

    changes

--- store_monitoring/models.py
+++ store_monitoring/models.py
@@ -28,3 +28,27 @@ class StoreStatus(models.Model):
 
     class Meta:
         db_table = 'store_status'
+
+
+class StoreResult(models.Model):
+    store_id = models.IntegerField()
+    uptime_last_hour = models.IntegerField()
+    uptime_last_day = models.IntegerField()
+    uptime_last_week = models.IntegerField()
+    downtime_last_hour = models.IntegerField()
+    downtime_last_day = models.IntegerField()
+    downtime_last_week = models.IntegerField()
+    uuid_associated = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'store_result'
+
+
+class UuidMapping(models.Model):
+    uuid = models.IntegerField()
+    status = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'uuid_mapping'
--- store_monitoring/report.py
+++ store_monitoring/report.py
@@ -1,13 +1,17 @@
 import asyncio
 from django.http import HttpResponse
 from django.views import View
-from store_monitoring.models import StoreStatus, TimeZone, MenuHours
+from store_monitoring.models import StoreStatus, TimeZone, MenuHours, StoreResult, UuidMapping
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.db import connections, connection
 from django.http import HttpResponse
 from ulid import ULID
 import time, datetime
+from asgiref.sync import sync_to_async
+import tracemalloc
+
+tracemalloc.start()
 
 
 @method_decorator(csrf_exempt, name="dispatch")
@@ -16,14 +20,36 @@ class create_report(View):
     async def post(self, request):
         unique_id = ULID.from_timestamp(time.time())
         print('getting the async function start', unique_id)
-        asyncio.create_task(self.process_data(unique_id))
+        async_function = self.process_data(unique_id)
+        loop = asyncio.get_event_loop()
+        loop.create_task(async_function)
+        # sync_to_async(async_function)  # or async_function
+        # asyncio.to_thread(async_function)
         return HttpResponse('ok')
 
+
     async def process_data(self, unique_id):
-        await asyncio.sleep(10)
-        print('inside the async function', unique_id)
+        try:
+            UuidMap = UuidMapping()
+            result = StoreResult()
+            UuidMap.uuid = unique_id
+            instance = UuidMap.save()
+            id = instance[0].id
+            calcutateLastHourUptime(id)
+            calculateLastDayUptime(id)
+            calculateLastWeekUptime(id)
+            calculateLastHourDowntime(id)
+            calculateLastWeekDowntime(id)
+            calculateLastDayDowntime(id)
+
+            print('inside the async function', unique_id)
+        except Exception as e:
+            print(e)
         return "task_complete"
 
+
+    def calculateLastHourUptime():
+        StoreStatus.object.filter()
 
 @method_decorator(csrf_exempt, name="dispatch")
 class get_report(View):
